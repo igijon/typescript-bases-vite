1e67a647284178662671a2f17e9f13b3dff5d037
Inma Gijón <igijoninf@gmail.com>
2024-12-08 10:25:51 +0100

    Optimización con desestructuración

--- src/topics/06-function-destructuring.ts
+++ src/topics/06-function-destructuring.ts
@@ -18,23 +18,27 @@ interface TaxCalculationOptions {
     products: Product[];
 }
 
-function taxCalculation( options: TaxCalculationOptions ): number[] {
+// function taxCalculation( options: TaxCalculationOptions ): [number, number] {
+// function taxCalculation( {tax, products}: TaxCalculationOptions ): [number, number] {
+function taxCalculation( options: TaxCalculationOptions ): [number, number] {
+    const {tax, products} = options;
+    
     let total = 0;
 
-    options.products.forEach( product => {
-        total += product.price;
+    products.forEach( ({price}) => {
+        total += price;
     });
 
-    return [total, total*options.tax];
+    return [total, total*tax];
 }
 
 const shoppingCart = [phone, tablet];
 const tax = 0.21;
 
-const result = taxCalculation({
+const [total, taxTotal] = taxCalculation({
     products: shoppingCart,
     tax
 });
 
-console.log('Total: ', result[0]);
-console.log('Tax: ', result[1]);
+console.log('Total: ', total);
+console.log('Tax: ', taxTotal);
