97755a7faea0ba0b7841fc8611378d3d7ebe21b5
Inma Gijón <igijoninf@gmail.com>
2024-12-08 09:22:21 +0100

    Funciones con objetos como argumentos

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -12,8 +12,30 @@ function multiply(firstNumber: number, secondNumber?: number, base: number = 2) 
     return firstNumber*base;
 }
 
-const result: number = addNumbers(1, 2);
-const result2: string = addNumbersArrow(1, 2);
-const multiplyResult: number = multiply(5);
+// const result: number = addNumbers(1, 2);
+// const result2: string = addNumbersArrow(1, 2);
+// const multiplyResult: number = multiply(5);
 
-console.log({result, result2, multiplyResult}); //Lo muiestra como un objeto
+// con
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount:number) => {
+    character.hp += amount; //Le sumamos a los puntos de vida el amount
+
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida ${this.hp}`);
+    }
+}
+
+healCharacter(strider, 10);
+healCharacter(strider, 50);
+strider.showHp();
