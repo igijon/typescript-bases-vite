ea203bce0367e081f93345608ed33b252d100f11
Inma Gijón <igijoninf@gmail.com>
2024-12-08 10:54:03 +0100

    Clases y constructores en TS

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -1,12 +1,17 @@
 export class Person {
-    public name: string;
-    private address: string;
+    // public name: string;
+    // private address: string;
 
-    constructor() {
-        this.name = 'Inma';
-        this.address = 'Ciudad Real'
-    }
+    // constructor(name: string, address: string) {
+    //     this.name = name;
+    //     this.address = address;
+    // }
+
+    constructor(
+        public name: string, 
+        private address: string = 'No address'
+    ) {}
 }
 
-const valquiria = new Person();
+const valquiria = new Person('Valquiria', 'New York');
 console.log(valquiria);
