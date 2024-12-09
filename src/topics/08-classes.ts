e35a8133d7042a6d23bd14bb20ba64d21ca16c63
Inma Gijón <igijoninf@gmail.com>
2024-12-09 12:18:32 +0100

    Composición sobre Herencia. Inyección de dependencias.

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -13,16 +13,34 @@ export class Person {
     ) {}
 }
 
-export class Heroin extends Person {
+// export class Heroin extends Person {
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string
+//     ) {
+//         super(realName, 'New York');
+//     }
+
+// }
+
+/** Composición: tenemos una dependencia en la clase Heroin, podemos ver la información
+ * con el atributo persona.
+ */
+export class Heroin {
     constructor(
         public alterEgo: string,
         public age: number,
-        public realName: string
+        public realName: string,
+        public person: Person // De esta manera si modifico la clase persona, no nos afecta
     ) {
-        super(realName, 'New York');
+        this.person = new Person(realName);
     }
 
 }
 
-const valquiria = new Heroin('Valquiria', 25, 'Brunilda');
+const bruni = new Person('Brunilda', 'New York'); 
+//yo puedo manejar esta instancia y mandarlo a las otras clases mediante lo que se llama
+//Inyección de dependencias y seguir reutilizando el código.
+const valquiria = new Heroin('Valquiria', 25, 'Brunilda', bruni);
 console.log(valquiria);
